06870e5c0eb40a55115c36fd876bb649c1d0ac45
Kowshik <kowshikk606@gmail.com>
2025-08-16 16:37:36 +0530

    Implemetation of quiz app

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import Quiz from './Quiz'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      {/* <App /> */}
       <Quiz/>
     </BrowserRouter>
   </React.StrictMode>
