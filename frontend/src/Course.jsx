34ddc52957556a9db6b98e323ce0ca68b3044ab4
Kowshik <kowshikk606@gmail.com>
2025-09-23 19:07:22 +0530

    courses

--- frontend/src/Course.jsx
+++ frontend/src/Course.jsx
@@ -1,24 +1,19 @@
-import { useState } from "react";
 import CoursesNavbar from "./components/CourceNavbar";
-import { Route,Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import CourseLanding from "./pages/Courses/CourceLanding";
 import Subjects from "./pages/Courses/Subjects";
 
-
 export default function LandingPage() {
-  const [dark, setDark] = useState(false);
-
   return (
-    <div >
-      <CoursesNavbar/>
+    <div>
+      <CoursesNavbar />
 
-        
-        <main className="flex-1 container mx-auto p-4">
+      <main className="flex-1 container mx-auto p-4">
         <Routes>
-          <Route index element={<CourseLanding/>} />
+          <Route index element={<CourseLanding />} />
           <Route path="category/:subject" element={<Subjects />} />
         </Routes>
-        </main>
-      </div>
+      </main>
+    </div>
   );
 }
