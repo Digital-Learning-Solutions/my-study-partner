76e94d785e2d46201c1ce903da0f5f1ecfd05414
Kowshik <kowshikk606@gmail.com>
2025-11-29 17:58:14 +0530

    fix import bug

--- backend/controllers/user/authController.js
+++ backend/controllers/user/authController.js
@@ -1,6 +1,6 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import User from "../../models/UserModel/User.js";
+import User from "../../models/UserModel/user.js";
 
 const JWT_SECRET = process.env.JWT_SECRET || "supersecretkey";
 
